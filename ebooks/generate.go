7c197ac2d5def233709d1edaf5355e8df62cc5d1
Kane York <kyork@student.42.us.org>
2016-10-12 00:07:38 -0700

    Update kayda.yml, and fix bindata

--- ebooks/generate.go
+++ ebooks/generate.go
@@ -3,5 +3,4 @@
 
 package ebooks
 
-//go:generate go-bindata -nomemcopy -debug content.opf cover.html part.html story.css toc.ncx
-//go:generate sed -i "s/package main/package ebooks/" bindata.go
+//go:generate go-bindata -nomemcopy -pkg ebooks content.opf cover.html part.html story.css toc.ncx
